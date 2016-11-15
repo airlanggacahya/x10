26799f8c2c2f5d3a7df2fd39f9f61bdab6efa08e
haibudi <budianto55@gmail.com>
2016-11-15 10:18:49 +0700

    comit authentication send rolename to ui

--- webapps/controllers/dashboard.go
+++ webapps/controllers/dashboard.go
@@ -28,6 +28,7 @@ func (c *DashboardController) Default(k *knot.WebContext) interface{} {
 		DataAccess.Menuname = o["Menuname"].(string)
 		DataAccess.Approve = o["Approve"].(bool)
 		DataAccess.Username = o["Username"].(string)
+		DataAccess.Rolename = o["Rolename"]
 	}
 
 	// if access != nil {

d86587ae96133200e9ac8a46a3c302a248d23a82
haibudi <budianto55@gmail.com>
2016-11-21 17:09:57 +0700

    comit page Edit Cibil detail add address details

--- webapps/assets/core/cibiltransitory.js
+++ webapps/assets/core/cibiltransitory.js
@@ -150,7 +150,12 @@ trans.showProm = function(Id){
 		trans.CurrentData().Telephones(tele)
 		trans.CurrentData().EmailAddress(email)
 
-		$(".modal-edit-bro").modal("show");
+		$(".modal-edit-bro").modal({
+			backdrop: 'static',
+    		keyboard: false,
+    		show: true
+		});
+		trans.loadAddress(trans.CurrentData().AddressData())
 
 		if($("#dateofreport").getKendoDatePicker() == undefined){
 			$("#dateofreport").kendoDatePicker({ format : "dd-MMM-yyyy" });
@@ -162,6 +167,93 @@ trans.showProm = function(Id){
 	    mask: "00:00:00",
 	    width:"100%"
 	});
+}
+
+trans.loadAddress = function(data){
+	var getData = ko.mapping.toJS(data)
+	$("#cibilAddress").html('');
+	$("#cibilAddress").kendoGrid({
+		dataSource: {
+			data: getData,
+			schema:{
+				model:{
+					id: "AddressPinCode",
+					fields: {
+						AddressPinCode: {editable : true},
+						Category: {editable : true},
+						DateReported: {editable: true}
+					}
+				}
+			}
+		},
+		dataBound: function () {
+			app.gridBoundTooltipster('#cibilAddress')()	
+		},
+		editable: true,
+		navigatable: true,
+		batch: true,
+		columns:[
+			{
+				field: "AddressPinCode",
+				title: "Address (With Pincode)",
+				headerAttributes: { "class": "sub-bgcolor" },
+				// width: 200,
+			},
+			{
+				field: "Category",
+				title: "Address Category",
+				headerAttributes: { "class": "sub-bgcolor" },
+				// width: 200,
+			},
+			{
+				field: "DateReported",
+				title: "Address Date Reported",
+				headerAttributes: { "class": "sub-bgcolor" },
+				template: function(d){
+					if(d.DateReported == ""){
+						return ""
+					}
+					return kendo.toString(new Date(d.DateReported), "dd-MMM-yyyy")
+				},
+				editor: trans.loadDateInput
+				// width: 200,
+			},
+			{
+				title: "<button class='btn btn-xs btn-flat btn-success tooltipster' title='Add Address' onclick='trans.addAddress()'><i class='fa fa-plus'></i></button>",
+				headerAttributes: { "class": "sub-bgcolor" },
+				template: function(d){
+					return "<button class='btn btn-xs btn-flat btn-danger tooltipster' title='Remove Address' onclick='trans.removeAddress(\""+d.uid+"\")'><i class='fa fa-trash'></i></button>"
+				},
+				width: 30,
+			}
+		]
+	})
+}
+
+trans.loadDateInput = function(container, options){
+	$('<input data-text-field="text" data-value-field="value" data-bind="value:' + options.field + '"/>')
+		.appendTo(container)
+		.kendoDatePicker({
+			format: 'dd-MMM-yyyy',
+	})
+}
+
+trans.addAddress = function(){
+	var data = $('#cibilAddress').data('kendoGrid').dataSource.data();
+	var newRow = {
+		AddressPinCode: "",
+		Category: "",
+		DateReported: "",
+	}
+
+	data.push(newRow);
+}
+
+trans.removeAddress = function(d){
+	var index = $('.cibilAddress tr[data-uid="'+d+'"]').index();
+	var allData = $('#cibilAddress').data('kendoGrid').dataSource.data();
+	// console.log(allData);
+	allData.splice(index, 1);
 }
 
 // trans.UpdateGrid = function(datas){
@@ -178,7 +270,7 @@ trans.SaveCibil = function(){
 	var param = {};
 	
 	var info = ko.mapping.toJS(trans.CurrentData)
-
+	var gridData = $("#cibilAddress").data("kendoGrid").dataSource.data()
 	param = info;
 	param.EmailAddress = param.EmailAddress.split("\n");
 	param.ScoringFactor = param.ScoringFactor.split("\n");
@@ -191,7 +283,28 @@ trans.SaveCibil = function(){
 	// param.TotalEnquiries30Days = parseFloat(param.TotalEnquiries30Days);
 	param.TotalOverdues = parseFloat(param.TotalOverdues);
 	param.TotalZeroBalanceAcc = parseFloat(param.TotalZeroBalanceAcc);
-
+	param.AddressData = [];
+	$.each(gridData, function(i, items){
+		var date = items.DateReported;
+		if( typeof date == "string"){
+			param.AddressData.push(
+				{
+					AddressPinCode: items.AddressPinCode,
+					Category: items.Category,
+					DateReported: items.DateReported,
+				}
+			)
+		}else{
+			param.AddressData.push(
+				{
+					AddressPinCode: items.AddressPinCode,
+					Category: items.Category,
+					DateReported: moment(items.DateReported,"hh:mm:ss").toDate().toISOString(),
+				}
+			)
+		}
+		
+	})
 	var tel = [];
 	param.Telephones.forEach(function(x){
 		var temp = x.split("-");
@@ -211,11 +324,13 @@ trans.SaveCibil = function(){
 	param.ConsumersInfos.DateOfBirth = moment(trans.CurrentData().ConsumersInfos.DateOfBirth()).toDate().toISOString()
 	param.DateOfReport = moment(trans.CurrentData().DateOfReport()).toDate().toISOString()
 	param.TimeOfReport = moment(trans.CurrentData().TimeOfReport(),"hh:mm:ss").toDate().toISOString()
+	// console.log("--------> param", param);
 	 ajaxPost(url, param, function(data) {
 	    if(data.Message!=""){
 	    	swal("Error",data.Message,"error");
 	    	return;
 	    }
+	    swal("Data Update Succefully", "", "success")
 	    $(".modal-edit-bro").modal("hide");
 	    trans.GetDataGrid();
 	})
@@ -225,7 +340,7 @@ trans.SaveCibil = function(){
 function GetCustomer(){
 	var url = "/datacapturing/getcustomerprofilelist";
 	  ajaxPost(url, "", function(data) {
-	     filter().CustomerSearchAll(data);
+	    filter().CustomerSearchAll(data);
 		trans.GetDataGrid();
 	});
 }

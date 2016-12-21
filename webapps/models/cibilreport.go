c4505b8f07b2693c19792c466b509adc2ea2154c
haibudi <budianto55@gmail.com>
2016-12-21 16:17:17 +0700

    comit backend cibil company

--- webapps/models/cibilreport.go
+++ webapps/models/cibilreport.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	. "eaciit/x10/webapps/connection"
+	// "fmt"
 	"github.com/eaciit/dbox"
 	"github.com/eaciit/orm"
 	tk "github.com/eaciit/toolkit"
@@ -123,6 +124,32 @@ type CibilDraftModel struct {
 	AllConfirmTime      time.Time             `bson:"AllConfirmTime,omitempty"`
 }
 
+type CibilTempModel struct {
+	Id                  bson.ObjectId         `bson:"_id" , json:"_id"`
+	FilePath            string                `bson:"FilePath"`
+	FileName            string                `bson:"FileName"`
+	ReportType          string                `bson:"ReportType"`
+	IsMatch             bool                  `bson:"IsMatch"`
+	UnconfirmID         string                `bson:"UnconfirmID"`
+	Profile             Profile               `bson:"Profile"`
+	ReportSummary       ReportSummary         `bson:"ReportSummary"`
+	DetailReportSummary []DetailReportSummary `bson:"DetailReportSummary"`
+	CreditTypeSummary   []CreditTypeSummary   `bson:"CreditTypeSummary"`
+	EnquirySummary      EnquirySummary        `bson:"EnquirySummary"`
+	Status              int                   `bson:"Status"`
+	Rating              string                `bson:"Rating"`
+	AcceptRejectTime    time.Time             `bson:"AcceptRejectTime"`
+	AllConfirmTime      time.Time             `bson:"AllConfirmTime"`
+	IsFreeze            bool                  `bson:"isFreeze"`
+	IsConfirm           int                   `bson:"isConfirm"`
+	IsCibilDraft        bool                  `bson:"iscibildraft"`
+}
+
+func NewCibilTempModel() *CibilTempModel {
+	n := new(CibilTempModel)
+	return n
+}
+
 func NewCibilDraftModel() *CibilDraftModel {
 	m := new(CibilDraftModel)
 	return m
@@ -202,7 +229,7 @@ func (m *CibilReportModel) GetAllData() ([]CibilReportModel, error) {
 	return data, err
 }
 
-func (m *CibilReportModel) GetData(cust int, dealno string) (CibilReportModel, error) {
+func (m *CibilReportModel) GetDataReport(cust int, dealno string) (CibilReportModel, error) {
 
 	data := CibilReportModel{}
 
@@ -237,12 +264,49 @@ func (m *CibilReportModel) GetData(cust int, dealno string) (CibilReportModel, e
 	return data, err
 }
 
+func (m *CibilDraftModel) GetDataDraft(cust int, dealno string) (CibilDraftModel, error) {
+
+	data := CibilDraftModel{}
+
+	conn, err := GetConnection()
+	defer conn.Close()
+	if err != nil {
+		return data, err
+	}
+
+	query := []*dbox.Filter{}
+	query = append(query, dbox.Eq("Profile.customerid", cust))
+	query = append(query, dbox.Eq("Profile.dealno", dealno))
+	csr, err := conn.NewQuery().
+		Where(query...).
+		From("CibilDraft").
+		Cursor(nil)
+
+	// _ = csr
+	if err != nil {
+		panic(err)
+	} else if csr == nil {
+		panic(csr)
+	}
+
+	defer csr.Close()
+
+	err = csr.Fetch(&data, 1, false)
+	if err != nil {
+		return data, err
+	}
+
+	return data, err
+}
+
 func (m *CibilReportModel) GetAllDataByParam(param tk.M) (tk.Result, int) {
 	conn, _ := GetConnection()
 	defer conn.Close()
 
 	res := tk.Result{}
-	data := []CibilReportModel{}
+	dataReport := []CibilReportModel{}
+	dataDraft := []CibilDraftModel{}
+	// temp := []CibilTempModel{}
 
 	query := []*dbox.Filter{}
 	query = append(query, dbox.Ne("_id", ""))
@@ -279,23 +343,90 @@ func (m *CibilReportModel) GetAllDataByParam(param tk.M) (tk.Result, int) {
 		Cursor(nil)
 	defer csr.Close()
 
+	csr1, err1 := conn.NewQuery().
+		From("CibilDraft").
+		Where(dbox.And(query...)).
+		Skip(param.GetInt("skip")).
+		Take(param.GetInt("take")).
+		Cursor(nil)
+	defer csr1.Close()
+
 	if err != nil {
 		res.SetError(err)
 	}
 
-	err = csr.Fetch(&data, 0, false)
+	err = csr.Fetch(&dataReport, 0, false)
+	err1 = csr1.Fetch(&dataDraft, 0, false)
 	if err != nil {
 		res.SetError(err)
 	}
 
-	res.SetData(data)
+	if err1 != nil {
+		res.SetError(err1)
+	}
+
+	arr := []*CibilTempModel{}
+
+	for _, a := range dataReport {
+		w := NewCibilTempModel()
+		w.Id = a.Id
+		w.FilePath = a.FilePath
+		w.FileName = a.FileName
+		w.ReportType = a.ReportType
+		w.IsMatch = a.IsMatch
+		w.UnconfirmID = a.UnconfirmID
+		w.Profile = a.Profile
+		w.ReportSummary = a.ReportSummary
+		w.DetailReportSummary = a.DetailReportSummary
+		w.CreditTypeSummary = a.CreditTypeSummary
+		w.EnquirySummary = a.EnquirySummary
+		w.Status = a.Status
+		w.Rating = a.Rating
+		w.AcceptRejectTime = a.AcceptRejectTime
+		w.AllConfirmTime = a.AllConfirmTime
+		w.IsFreeze = a.IsFreeze
+		w.IsConfirm = a.IsConfirm
+		w.IsCibilDraft = false
+
+		arr = append(arr, w)
+
+	}
+
+	for _, o := range dataDraft {
+		l := NewCibilTempModel()
+		l.Id = o.Id
+		l.FilePath = o.FilePath
+		l.FileName = o.FileName
+		l.ReportType = o.ReportType
+		// l.IsMatch = o.IsMatch
+		// l.UnconfirmID = o.UnconfirmID
+		l.Profile = o.Profile
+		l.ReportSummary = o.ReportSummary
+		l.DetailReportSummary = o.DetailReportSummary
+		l.CreditTypeSummary = o.CreditTypeSummary
+		l.EnquirySummary = o.EnquirySummary
+		l.Status = o.Status
+		// l.Rating = o.Rating
+		l.AcceptRejectTime = o.AcceptRejectTime
+		l.AllConfirmTime = o.AllConfirmTime
+		// l.IsFreeze = o.IsFreeze
+		// l.IsConfirm = o.IsConfirm
+		l.IsCibilDraft = true
+
+		arr = append(arr, l)
+
+	}
+
+	res.SetData(arr)
+
+	// re ,_ := m.GetTotalRow(query)
 
 	total, _ := m.GetTotalRow(query)
 
 	return res, total
 }
 
-func (m *CibilReportModel) Update(data CibilReportModel) error {
+func (m *CibilReportModel) UpdateReport(data CibilReportModel) error {
 
 	conn, err := GetConnection()
 	defer conn.Close()
@@ -305,6 +436,30 @@ func (m *CibilReportModel) Update(data CibilReportModel) error {
 
 	qinsert := conn.NewQuery().
 		From("CibilReport").
+		SetConfig("multiexec", true).
+		Save()
+
+	defer qinsert.Close()
+
+	insdata := map[string]interface{}{"data": data}
+	err = qinsert.Exec(insdata)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func (m *CibilReportModel) UpdateDraft(data CibilReportModel) error {
+
+	conn, err := GetConnection()
+	defer conn.Close()
+	if err != nil {
+		return err
+	}
+
+	qinsert := conn.NewQuery().
+		From("CibilDraft").
 		SetConfig("multiexec", true).
 		Save()
 

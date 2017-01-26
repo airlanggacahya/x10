1124ca597e2932e6bf914b32224db10456e03a49
Airlangga Cahya Utama <airlangga.cahya@eaciit.com>
2017-01-26 16:06:03 +0700

    Implement import stakeholder. Please note there is possibility of name duplication. We remove duplicate name here.

---
+++ consoleapps/OmnifinMaster/stakeholdertype.go
@@ -0,0 +1,33 @@
+package main
+
+import tk "github.com/eaciit/toolkit"
+
+// transformScheme save additional information specific for master Scheme
+func transformStakeholderType(in tk.M) tk.M {
+	// tk.Printfn("%v", in)
+	p := make(tk.M)
+	// copy all elements
+	for key, val := range in {
+		p[key] = val
+	}
+
+	p["name"] = p["description"]
+
+	return p
+}
+
+// SaveScheme transform remote data master scheme desc into master account detail
+func SaveStakeholderType(data interface{}) error {
+	newStake := TransformMaster("genericMasterList", data, transformStakeholderType)
+	// debug
+	// tk.Printfn("%v", newStake)
+	newStake = removeDuplicateStringField(newStake, "name")
+	// tk.Printfn("%v", newStake)
+	err := SaveMasterAccountDetail("Position", newStake)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}

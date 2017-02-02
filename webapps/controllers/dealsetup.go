aa9dfe48180754e8edd079c5ddc1d542ad3058ce
DermawanCahyoUtomo <dermawan.utomo@gmail.com>
2017-02-02 10:45:31 +0700

    add function get all data deal setup and create model deal setup

--- webapps/controllers/dealsetup.go
+++ webapps/controllers/dealsetup.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	// "github.com/eaciit/dbox"
+	// . "eaciit/x10/webapps/connection"
+	. "eaciit/x10/webapps/models"
 	"github.com/eaciit/knot/knot.v1"
+	tk "github.com/eaciit/toolkit"
 )
 
 type DealSetUpController struct {
@@ -34,4 +38,21 @@ func (c *DealSetUpController) Default(k *knot.WebContext) interface{} {
 	k.Config.IncludeFiles = []string{"shared/filter.html", "shared/loading.html", "dealsetup/top.html", "dealsetup/bottom.html"}
 
 	return DataAccess
+}
+
+func (c *DealSetUpController) GetAllDataDealSetup(k *knot.WebContext) interface{} {
+	k.Config.OutputType = knot.OutputJson
+
+	csr, err := c.Ctx.Find(new(DealSetupModel), tk.M{})
+	if err != nil {
+		return c.ErrorResultInfo(err.Error(), nil)
+	}
+	result := make([]DealSetupModel, 0)
+	err = csr.Fetch(&result, 0, false)
+	if err != nil {
+		return c.ErrorResultInfo(err.Error(), nil)
+	}
+	csr.Close()
+
+	return result
 }

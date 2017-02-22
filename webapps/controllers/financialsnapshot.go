cc370c551c137a9d202bc9d674180d1a66f8471b
Airlangga Cahya Utama <airlangga.cahya@eaciit.com>
2017-02-22 18:15:18 +0700

    Implement financialsnapshot and creditscorecard

--- webapps/controllers/financialsnapshot.go
+++ webapps/controllers/financialsnapshot.go
@@ -25,26 +25,17 @@ func (c *FinancialSnapshotController) Default(k *knot.WebContext) interface{} {
 	access := c.LoadBase(k)
 	k.Config.NoLog = true
 	k.Config.OutputType = knot.OutputTemplate
-	DataAccess := Previlege{}
-
-	for _, o := range access {
-		DataAccess.Create = o["Create"].(bool)
-		DataAccess.View = o["View"].(bool)
-		DataAccess.Delete = o["Delete"].(bool)
-		DataAccess.Process = o["Process"].(bool)
-		DataAccess.Delete = o["Delete"].(bool)
-		DataAccess.Edit = o["Edit"].(bool)
-		DataAccess.Menuid = o["Menuid"].(string)
-		DataAccess.Menuname = o["Menuname"].(string)
-		DataAccess.Approve = o["Approve"].(bool)
-		DataAccess.Username = o["Username"].(string)
-		DataAccess.Fullname = o["Fullname"].(string)
-	}
-
+	DataAccess := NewPrevilege(access)
 	DataAccess.TopMenu = c.GetTopMenuName(DataAccess.Menuname)
 
 	k.Config.OutputType = knot.OutputTemplate
-	k.Config.IncludeFiles = []string{"shared/filter.html", "shared/loading.html", "financialsnapshot/kiri.html", "financialsnapshot/kanan.html"}
+	k.Config.IncludeFiles = []string{
+		"shared/dataaccess.html",
+		"shared/filter.html",
+		"shared/loading.html",
+		"financialsnapshot/kiri.html",
+		"financialsnapshot/kanan.html",
+	}
 
 	return DataAccess
 }

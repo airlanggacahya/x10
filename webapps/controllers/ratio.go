7e9ce2e53625c8dc54c17cce8c3cf6a3d5792cd8
Airlangga Cahya Utama <airlangga.cahya@eaciit.com>
2017-02-23 10:30:55 +0700

    Implement grant on detail finance report

--- webapps/controllers/ratio.go
+++ webapps/controllers/ratio.go
@@ -21,27 +21,15 @@ func (c *RatioController) Input(k *knot.WebContext) interface{} {
 	access := c.LoadBase(k)
 	k.Config.NoLog = true
 	k.Config.OutputType = knot.OutputTemplate
-	DataAccess := Previlege{}
-
-	for _, o := range access {
-		DataAccess.Create = o["Create"].(bool)
-		DataAccess.View = o["View"].(bool)
-		DataAccess.Delete = o["Delete"].(bool)
-		DataAccess.Process = o["Process"].(bool)
-		DataAccess.Delete = o["Delete"].(bool)
-		DataAccess.Edit = o["Edit"].(bool)
-		DataAccess.Menuid = o["Menuid"].(string)
-		DataAccess.Menuname = o["Menuname"].(string)
-		DataAccess.Approve = o["Approve"].(bool)
-		DataAccess.Username = o["Username"].(string)
-		DataAccess.Rolename = o["Rolename"].(string)
-		DataAccess.Fullname = o["Fullname"].(string)
-	}
-
+	DataAccess := NewPrevilege(access)
 	DataAccess.TopMenu = c.GetTopMenuName(DataAccess.Menuname)
 
 	k.Config.OutputType = knot.OutputTemplate
-	k.Config.IncludeFiles = []string{"shared/filter.html", "shared/loading.html"}
+	k.Config.IncludeFiles = []string{
+		"shared/dataaccess.html",
+		"shared/filter.html",
+		"shared/loading.html",
+	}
 
 	return DataAccess
 }
@@ -130,26 +118,15 @@ func (c *RatioController) Report(k *knot.WebContext) interface{} {
 	access := c.LoadBase(k)
 	k.Config.NoLog = true
 	k.Config.OutputType = knot.OutputTemplate
-	DataAccess := Previlege{}
-
-	for _, o := range access {
-		DataAccess.Create = o["Create"].(bool)
-		DataAccess.View = o["View"].(bool)
-		DataAccess.Delete = o["Delete"].(bool)
-		DataAccess.Process = o["Process"].(bool)
-		DataAccess.Delete = o["Delete"].(bool)
-		DataAccess.Edit = o["Edit"].(bool)
-		DataAccess.Menuid = o["Menuid"].(string)
-		DataAccess.Menuname = o["Menuname"].(string)
-		DataAccess.Approve = o["Approve"].(bool)
-		DataAccess.Username = o["Username"].(string)
-		DataAccess.Fullname = o["Fullname"].(string)
-	}
-
+	DataAccess := NewPrevilege(access)
 	DataAccess.TopMenu = c.GetTopMenuName(DataAccess.Menuname)
 
 	k.Config.OutputType = knot.OutputTemplate
-	k.Config.IncludeFiles = []string{"shared/filter.html", "shared/loading.html"}
+	k.Config.IncludeFiles = []string{
+		"shared/dataaccess.html",
+		"shared/filter.html",
+		"shared/loading.html",
+	}
 
 	return DataAccess
 }

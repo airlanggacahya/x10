b89211156a7951ebc0c5dbc9d089daf32e5c0327
yanda15 <yanda151092@gmail.com>
2017-03-14 10:19:38 +0700

    dashboard model

--- webapps/models/dashboard.go
+++ webapps/models/dashboard.go
@@ -14,16 +14,27 @@ type DashboardFilterModel struct {
 	}
 }
 
-func NewDashboardFilterModel() *DashboardFilterModel {
-	m := new(DashboardFilterModel)
-	m.Id = ""
-	return m
-}
-
 func (e *DashboardFilterModel) RecordID() interface{} {
 	return e.Id
 }
 
 func (m *DashboardFilterModel) TableName() string {
 	return "DashboardFilter"
+}
+
+type DashboardNoteModel struct {
+	orm.ModelBase `bson:"-",json:"-"`
+	Id            string ` bson:"_id" , json:"_id" ` //mas Bud ini sampean isi UserID
+	Comments      []struct {
+		Text    string
+		Checked bool
+	}
+}
+
+func (m *DashboardNoteModel) TableName() string {
+	return "DashboardNotes"
+}
+
+func (e *DashboardNoteModel) RecordID() interface{} {
+	return e.Id
 }

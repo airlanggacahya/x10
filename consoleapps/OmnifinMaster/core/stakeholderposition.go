9401ca6d127e8d6690e77c9b7e83c19336eb9b0a
yanda15 <yanda151092@gmail.com>
2017-03-14 19:38:57 +0700

    omnifin master

---
+++ consoleapps/OmnifinMaster/core/stakeholderposition.go
@@ -0,0 +1,26 @@
+package core
+
+import (
+	hp "eaciit/x10/webapps/helper"
+
+	tk "github.com/eaciit/toolkit"
+)
+
+func SaveStakeholderPosition(data []GenericMaster) error {
+	newData := []tk.M{}
+	for _, val := range data {
+		p := tk.M{}
+		p.Set("name", hp.ToWordCase(val.Description))
+		p.Set("description", val.Description)
+		p.Set("code", val.Code)
+		p.Set("genericKey", val.GenericKey)
+		// tk.Printfn("%v", p)
+		newData = append(newData, p)
+	}
+
+	newData = removeDuplicateStringField(newData, "name")
+
+	err := SaveMasterAccountDetail("StakeholderPosition", newData)
+
+	return err
+}

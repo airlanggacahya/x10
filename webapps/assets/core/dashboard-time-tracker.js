cb6c9aa478bd3803ebb79bc2a2f042e2967dc547
haibudi <budianto55@gmail.com>
2017-03-16 16:40:49 +0700

    comit deal tracker

--- webapps/assets/core/dashboard-time-tracker.js
+++ webapps/assets/core/dashboard-time-tracker.js
@@ -1,6 +1,89 @@
 var ttrack = {};
+ttrack.trackingList = ko.observableArray([
+    {text : 'Deal Region', value: 'region'},
+    {text : 'Deal Stages', value: 'stages'}
+]);
+ttrack.trackingValue = ko.observable("stages");
+ttrack.modalChartTittle = ko.observable("");
+ttrack.isRegion = ko.observable(false);
+ttrack.modalGridTittle = ko.observable("");
+ttrack.DataGridModal = ko.observableArray([])
+ttrack.popupchartcolor = ko.observableArray([])
 
 ttrack.chartcolors = ["#4472C4","#70AD47","#FFC000","#FF0000"];
+ttrack.renderModalChart = function(datas){
+    datas = ttrack.normalisasiData(datas)
+
+    datas = _.sortBy(datas,["status"])
+
+    var stocksDataSource = new kendo.data.DataSource({
+        data : datas,
+        group: {
+            field: "timestatus"
+        },
+    });
+    $("#chartRegion").html("");
+
+    if(datas.length == 0){
+        $("#chartRegion").html("Data not available..")
+    }
+
+    $("#chartRegion").kendoChart({
+        title: { 
+            text: ttrack.modalChartTittle(),
+            font:  "bold 12px Arial,Helvetica,Sans-Serif",
+            align: "center",
+            color: "#58666e",
+        },
+        dataSource: stocksDataSource,
+        series: [{
+            type: "column",
+            stack : false,
+            field: "count",
+            name: "#= group.value.split('*')[1] #"
+        }],
+        chartArea: {
+            width: 700,
+            height: 200
+        },
+        legend: {
+            visible: false
+        },
+        seriesColors : ttrack.popupchartcolor(),
+        valueAxis: {
+            labels: {
+                // format: "${0}",
+                font: "10px sans-serif",
+                skip: 2,
+                step: 2
+            },
+            title : {
+                text : "No. of Deals",
+                font: "11px sans-serif",
+                visible : true,
+                color : "#4472C4"
+            }
+        },
+        categoryAxis: {
+            field: "status",
+            title : {
+                 text : "Deal Stages",
+                font: "11px sans-serif",
+                visible : true,
+                color : "#4472C4"
+            },
+            labels : {
+                font: "10px sans-serif",
+            }
+        },
+        tooltip : {
+            visible: true,
+            template : function(dt){
+                return dt.dataItem.timestatus.split("*")[1] + " : " + dt.value
+            }
+        }
+    });
+}
 ttrack.renderChart = function(datas){
 			datas = ttrack.normalisasiData(datas)
 
@@ -19,7 +102,12 @@ ttrack.renderChart = function(datas){
 			}
 
 			$("#timeTrackerChart").kendoChart({
-                title: { text: "Stock Prices" },
+                title: { 
+                    text: "Deal-time Tracking",
+                    font:  "bold 14px Arial,Helvetica,Sans-Serif",
+                    align: "left",
+                    color: "#58666e",
+                },
                 dataSource: stocksDataSource,
                 series: [{
                     type: "column",
@@ -27,6 +115,30 @@ ttrack.renderChart = function(datas){
                     field: "count",
                     name: "#= group.value.split('*')[1] #"
                 }],
+                seriesClick : function(e){
+                    console.log(e)
+                    var status = e.dataItem.timestatus.split("*")[1];
+                    if(status == "Over due"){
+                        ttrack.popupchartcolor(["#FF0000"]);
+                    }else if( status == "New"){
+                        ttrack.popupchartcolor(["#4472C4"]);
+                    }else if(status == "Getting due"){
+                        ttrack.popupchartcolor(["#70AD47"]);
+                    }else if(status == "In time"){
+                        ttrack.popupchartcolor(["#FFC000"])
+                    }
+                    var str = e.dataItem.status+" : "+status+" Deals accross Stages";
+                    var gstr = "In Queue : "+ status+ " Deals"
+                    ttrack.modalGridTittle(gstr);
+
+                    ttrack.modalChartTittle(str);
+                    if(ttrack.trackingValue() == 'stages'){
+                        alert("Stages")
+                        ttrack.loadDataStages(e);
+                    }else if(ttrack.trackingValue() == 'region'){
+                        ttrack.loadDataRegion(e);
+                    }
+                },
                 legend: {
                     position: "right"
                 },
@@ -66,8 +178,49 @@ ttrack.renderChart = function(datas){
             });
 }
 
+ttrack.loadDataRegion = function(d){
+    ttrack.isRegion(true)
+    ttrack.DataGridModal([]);
+    var param ={
+        regionname: d.dataItem.status,
+        timestatus: d.dataItem.timestatus,
+    }
+    ajaxPost("/dashboard/timetrackergriddetails", param, function(res){
+        ttrack.DataGridModal(res.Data)
+        var onparam = {
+            "regionname" : d.dataItem.status,
+            "timestatus" : d.dataItem.timestatus
+        }
+        ajaxPost("/dashboard/timetracker", onparam,function(res){
+               ttrack.renderModalChart(res.Data)
+            
+        });
+        ttrack.loadDealGrid()
+
+    })
+}
+
+ttrack.loadDataStages = function(d){
+    ttrack.isRegion(false)
+    ttrack.DataGridModal([]);
+    var param ={
+        stagename: d.dataItem.status,
+        timestatus: d.dataItem.timestatus,
+    }
+    ajaxPost("/dashboard/timetrackergriddetails", param, function(res){
+        ttrack.DataGridModal(res.Data);
+        ttrack.loadDealGrid()
+    });
+    
+}
+
+
+
 ttrack.getData = function(){
-	ajaxPost("/dashboard/timetracker",{},function(res){
+    var param = {
+        groupby: ttrack.trackingValue(),
+    }
+	ajaxPost("/dashboard/timetracker", param,function(res){
 		   ttrack.renderChart(res.Data)
 		})
 	}
@@ -105,6 +258,60 @@ ttrack.normalisasiData = function(data){
       }
       console.log(data)
     return data
+}
+
+ttrack.loadDealGrid = function(){
+    console.log("nnnnnn----------",ttrack.DataGridModal())
+    $("#dealStatus").html('');
+    $("#dealStatus").kendoGrid({
+        dataSource: ttrack.DataGridModal(),
+        columns:[
+            {
+                field: "",
+                title: "Sr. No.",
+                headerAttributes: {style: "background: red; color: white"},
+                width: 50,
+
+            },
+            {
+                field: "custname",
+                title: "Customer Name",
+                headerAttributes: {style: "background: red; color: white"},
+                width: 120,
+
+            },
+            {
+                field: "dealno",
+                title: "DealNo",
+                headerAttributes: {style: "background: red; color: white"},
+                width: 120,
+
+            },
+            {
+                field: "caname",
+                title: "CA",
+                headerAttributes: {style: "background: red; color: white"},
+                width: 50,
+
+            },
+            {
+                field: "rmname",
+                title: "RM",
+                headerAttributes: {style: "background: red; color: white"},
+                width: 50,
+
+            },
+            {
+                field: "",
+                title: "Details",
+                headerAttributes: {style: "background: red; color: white"},
+                width: 120,
+
+            },
+        ]
+    });
+
+    $("#Deals").modal('show');
 }
 
 

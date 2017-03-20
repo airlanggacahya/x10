d93b48a532c9878c2093165262278b11a1d337e6
haibudi <budianto55@gmail.com>
2017-03-20 10:43:29 +0700

    fix bugs accordion

--- webapps/assets/core/dashboard-time-tracker.js
+++ webapps/assets/core/dashboard-time-tracker.js
@@ -331,15 +331,15 @@ ttrack.accordion = function(){
         if($this.next().children().hasClass('show')){
             $this.next().children().removeClass('show');
             $this.next().children().slideUp(500);
-            $this.find("h4>").removeClass("fa-chevron-down");
-            $this.find("h4>").addClass("fa-chevron-up");
+            $this.find("h5>").removeClass("fa-chevron-down");
+            $this.find("h5>").addClass("fa-chevron-up");
 
         }else{
             $this.next().children().removeClass('hide');
             $this.next().children().slideDown(500);
             $this.next().children().addClass("show");
-            $this.find("h4>").addClass("fa-chevron-down");
-            $this.find("h4>").removeClass("fa-chevron-up");
+            $this.find("h5>").addClass("fa-chevron-down");
+            $this.find("h5>").removeClass("fa-chevron-up");
         }
     })
 }
